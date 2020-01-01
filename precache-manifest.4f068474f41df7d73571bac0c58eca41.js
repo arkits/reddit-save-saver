eda275f9cace7b29bf8082dd9fc219589a779637
Archit Khode <arkits@outlook.com>
2020-01-01 22:53:39 +0530

    Updates

---
+++ precache-manifest.4f068474f41df7d73571bac0c58eca41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69135261cbed6d8cd8c744537af39f2",
+    "url": "/reddit-save-saver/index.html"
+  },
+  {
+    "revision": "22ac14bc179f456d01c2",
+    "url": "/reddit-save-saver/static/js/2.ca55ebc4.chunk.js"
+  },
+  {
+    "revision": "37ef80829e57f4a0c2aa4bfe70fe2cf3",
+    "url": "/reddit-save-saver/static/js/2.ca55ebc4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d614c8008aeabdf6ccb7",
+    "url": "/reddit-save-saver/static/js/main.a24e13d1.chunk.js"
+  },
+  {
+    "revision": "39f7815e5bab8a44f8b8",
+    "url": "/reddit-save-saver/static/js/runtime-main.b7961885.js"
+  }
+]);
