75f0aef914e4efc0ccdf9deb72e7acb8133656eb
Archit Khode <arkits@outlook.com>
2020-01-01 19:09:32 +0530

    added last_dump is debug endpoint + refactor

--- src/api/domain/debug.py
+++ src/api/domain/debug.py
@@ -3,22 +3,47 @@ import config
 import pymongo
 from domain import version
 
+
 # MongoDB Client
 dbClient = pymongo.MongoClient(config.MONGODB_URI)
 rssDb = dbClient['reddit_save_saver']
 
+
 def get_debug(username):
 
-    to_return = {
-        "username": username,
-    }
-
     logger.info("Received get_debug for username={}", username)
-    db_user_collection = rssDb[username]
 
-    n_posts = db_user_collection.find().count()
-    to_return["number_of_posts"] = n_posts
+    to_return = {}
+
+    to_return["username"] = username
+
+    to_return["number_of_posts"] = calc_number_of_posts(username)
+
+    to_return["last_dump"] = calc_last_dump()
 
     to_return["api"] = version.get_version()
 
     return to_return
+
+
+def calc_number_of_posts(username):
+
+    n_posts = 0
+
+    db_user_collection = rssDb[username]
+    n_posts = db_user_collection.find().count()
+
+    return n_posts
+
+
+def calc_last_dump():
+
+    last_dump_timestamp = 0
+
+    db_meta_collection = rssDb["db_meta"]
+    last_dump = db_meta_collection.find_one({"key":"last_dump"})
+
+    if last_dump is not None:
+        last_dump_timestamp = last_dump["value"]
+
+    return last_dump_timestamp
