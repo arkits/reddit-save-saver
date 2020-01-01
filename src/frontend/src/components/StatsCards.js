52c427aa92caa455c612662c5db88389b198c0e2
Archit Khode <arkits@outlook.com>
2020-01-01 21:41:18 +0530

    displaying last update with moment.js

--- src/frontend/src/components/StatsCards.js
+++ src/frontend/src/components/StatsCards.js
@@ -4,20 +4,37 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import Popover from "@material-ui/core/Popover";
+
+var moment = require("moment");
 
 const useStyles = makeStyles({
+    popover: {
+        pointerEvents: "none"
+    },
     title: {
         fontSize: 14
     }
 });
 
 function StatsCards({ data }) {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handlePopoverOpen = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handlePopoverClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
 
     const classes = useStyles();
 
     return (
         <Grid container spacing={1}>
-            <Grid item xs={6}>
+            <Grid item xs={4}>
                 <Card className={classes.card}>
                     <CardContent>
                         <Typography
@@ -33,7 +50,7 @@ function StatsCards({ data }) {
                     </CardContent>
                 </Card>
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={4}>
                 <Card className={classes.card}>
                     <CardContent>
                         <Typography
@@ -48,6 +65,49 @@ function StatsCards({ data }) {
                         </Typography>
                     </CardContent>
                 </Card>
+            </Grid>
+            <Grid
+                item
+                xs={4}
+                onMouseEnter={handlePopoverOpen}
+                onMouseLeave={handlePopoverClose}
+            >
+                <Card className={classes.card}>
+                    <CardContent>
+                        <Typography
+                            className={classes.title}
+                            color="textSecondary"
+                            gutterBottom
+                        >
+                            Last Update
+                        </Typography>
+                        <Typography variant="h6" component="h2">
+                            {moment(data.last_dump * 1000).fromNow()}
+                        </Typography>
+                    </CardContent>
+                </Card>
+
+                <Popover
+                    id="mouse-over-popover"
+                    className={classes.popover}
+                    classes={{
+                        paper: classes.paper
+                    }}
+                    open={open}
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                        vertical: "bottom",
+                        horizontal: "left"
+                    }}
+                    transformOrigin={{
+                        vertical: "top",
+                        horizontal: "left"
+                    }}
+                    onClose={handlePopoverClose}
+                    disableRestoreFocus
+                >
+                    <Typography>{moment(data.last_dump * 1000).format("dddd, MMMM Do YYYY, h:mm:ss a")}</Typography>
+                </Popover>
             </Grid>
         </Grid>
     );
